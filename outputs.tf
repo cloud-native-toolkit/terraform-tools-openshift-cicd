0986e8d9300bfd0493c26dba75b58e472bd16db8
Sean Sundberg <seansund@us.ibm.com>
2021-06-18 09:50:33 -0500

    Fixes output variable ref
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 
 output "argocd_namespace" {
   description = "The namespace where the ArgoCD instance has been provisioned"
-  value       = module.argocd.namespace
+  value       = module.gitops.namespace
 }
