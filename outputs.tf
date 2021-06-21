3c3fc9164ff3813a2e7a14a4a555a1602574c1be
Sean Sundberg <seansund@us.ibm.com>
2021-06-21 16:54:28 -0500

    Adds ArgoCD outputs to module (#3)
    - Bumps argocd module to v2.15.1 to get output variables
    - Adds host, url, username, and password from argocd output to module output
    - Adds ocp4.5 cluster to test matrix
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,26 @@ output "argocd_namespace" {
   description = "The namespace where the ArgoCD instance has been provisioned"
   value       = module.gitops.namespace
 }
+
+output "argocd_host" {
+  description = "The ingress host for the Argo CD instance"
+  value = module.gitops.ingress_host
+  sensitive = true
+}
+
+output "argocd_url" {
+  description = "The ingress url for the Argo CD instance"
+  value = module.gitops.ingress_url
+  sensitive = true
+}
+
+output "argocd_username" {
+  description = "The username of the default ArgoCD admin user"
+  value = module.gitops.username
+}
+
+output "argocd_password" {
+  description = "The password of the default ArgoCD admin user"
+  value = module.gitops.password
+  sensitive = true
+}
