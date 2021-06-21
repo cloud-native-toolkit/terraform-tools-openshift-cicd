3905ef27c146c2b703ba706eed182fee8c87c0d2
Sean Sundberg <seansund@us.ibm.com>
2021-06-21 15:16:39 -0500

    Adds ArgoCD outputs to module
    - Bumps argocd module to v2.15.1 to get output variables
    - Adds host, url, username, and password from argocd output to module output
    - Adds ocp4.5 cluster to test matrix
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "gitops" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-argocd.git?ref=v2.14.0"
+  source = "github.com/cloud-native-toolkit/terraform-tools-argocd.git?ref=v2.15.1"
 
   cluster_type        = var.cluster_type
   ingress_subdomain   = var.ingress_subdomain
