046673e2175aca96ad8e142b6575de37314ef558
Sean Sundberg <seansund@us.ibm.com>
2021-06-23 10:11:18 -0500

    Bumps argocd module to v2.16.0
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "gitops" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-argocd.git?ref=v2.15.1"
+  source = "github.com/cloud-native-toolkit/terraform-tools-argocd.git?ref=v2.16.0"
 
   cluster_type        = var.cluster_type
   ingress_subdomain   = var.ingress_subdomain
