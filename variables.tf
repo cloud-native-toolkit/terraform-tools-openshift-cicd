32b5048c80c53eb488fc043af7a49dc856c936e8
Sean Sundberg <seansund@us.ibm.com>
2021-07-09 09:46:56 -0500

    Adds sealed-secret module to cicd deployment
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- variables.tf
+++ variables.tf
@@ -28,3 +28,33 @@ variable "ingress_subdomain" {
   description = "The subdomain for ingresses in the cluster"
   default     = ""
 }
+
+variable "sealed_secret_public_key" {
+  type        = string
+  description = "The public key that will be used to encrypt sealed secrets. If not provided, a new one will be generated"
+  default     = ""
+}
+
+variable "sealed_secret_private_key" {
+  type        = string
+  description = "The private key that will be used to decrypt sealed secrets. If not provided, a new one will be generated"
+  default     = ""
+}
+
+variable "sealed_secret_public_key_file" {
+  type        = string
+  description = "The file containing the public key that will be used to encrypt the sealed secrets. If not provided a new public key will be generated"
+  default     = ""
+}
+
+variable "sealed_secret_private_key_file" {
+  type        = string
+  description = "The file containin the private key that will be used to encrypt the sealed secrets. If not provided a new private key will be generated"
+  default     = ""
+}
+
+variable "sealed_secret_namespace" {
+  type        = string
+  description = "The namespace where the sealed secret will be deployed"
+  default     = "sealed-secrets"
+}
