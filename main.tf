a890aa9def0c8889f7a973d8e8854f6963d1d52f
Sean Sundberg <seansund@us.ibm.com>
2022-03-07 12:59:45 -0600

    Updats tekton variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ module "pipelines" {
   source = "github.com/cloud-native-toolkit/terraform-tools-tekton.git?ref=v2.4.0"
 
   cluster_config_file_path = var.cluster_config_file
-  cluster_type             = var.cluster_type
   cluster_ingress_hostname = var.ingress_subdomain
   tools_namespace          = var.tools_namespace
   provision                = module.gitops.provision_tekton
