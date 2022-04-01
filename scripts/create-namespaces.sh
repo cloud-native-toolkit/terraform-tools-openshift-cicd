e2c634a8551ccb73c0667cf8027560c926b22a97
Sean Sundberg <seansund@us.ibm.com>
2022-04-01 01:56:07 -0500

    Creates tools and sealed secrets namespaces if missing (#36)
    closes #35
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ scripts/create-namespaces.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+NAMESPACES_JSON="$1"
+
+export PATH="${BIN_DIR}:${PATH}"
+
+echo "${NAMESPACES_JSON}" | jq -r '.[]' | while read namespace; do
+  if ! kubectl get namespace "${namespace}" 1> /dev/null 2> /dev/null; then
+    echo "Creating namespace: ${namespace}"
+
+    if kubectl get routes -A 1> /dev/null 2> /dev/null; then
+      oc new-project "${namespace}" 1> /dev/null || exit 1
+    else
+      kubectl create namespace "${namespace}" 1> /dev/null || exit 1
+    fi
+
+    oc label namespace "${namespace}" created-by=openshift-cicd
+  else
+    echo "Namespace already exists: ${namespace}"
+  fi
+done
