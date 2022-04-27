b67db4bcf747ae1027dd27f07bdebf8d171fb322
Sean Sundberg <seansund@us.ibm.com>
2022-04-27 14:30:59 -0500

    Updates argocd module to v2.18.10
    - Picks up fix to only install openshift-gitops operator
    
    closes #48
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
 
 
 module "gitops" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-argocd.git?ref=v2.18.9"
+  source = "github.com/cloud-native-toolkit/terraform-tools-argocd.git?ref=v2.18.10"
 
   cluster_config_file = var.cluster_config_file
   olm_namespace       = var.olm_namespace
